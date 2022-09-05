aa37b7a7ee6a48fa267fbaac364f5d0a52dcea83
Kuldeep Rawat <kuldeeprawat@virtualemployee.com>
2022-09-05 18:22:30 +0530

    first merge

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import 'font-awesome/css/font-awesome.min.css';
+
+function App() {
+  return (
+    <>
+      <h2>Hello World!</h2>
+    </>
+  );
+}
+
+export default App;
