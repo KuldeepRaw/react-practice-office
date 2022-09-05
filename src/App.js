4ba0ebaa0e22616435cd8485b58d6aba02c50da9
Kuldeep Rawat <kuldeeprawat@virtualemployee.com>
2022-09-05 18:43:02 +0530

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
+import logo from './logo.svg';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
-import 'font-awesome/css/font-awesome.min.css';
 
 function App() {
   return (
-    <>
-      <h2>Hello World!</h2>
-    </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
